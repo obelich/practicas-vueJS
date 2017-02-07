63bed5c09db650aeebb8b7459ef3b4325568ed3b
Obelich <obelich@gmail.com>
2017-02-06 20:57:40 -0800

    Se termino la practica del video 1 y como nota en la documentacion se elimino el $remove

--- js/todolist.js
+++ js/todolist.js
@@ -6,6 +6,7 @@ var app = new Vue({
   el: '#todo-list',
   data: {
     nombre: 'DO Podcast',
+    nuevaTarea: '',
     tareas: [
       {nombre: 'Hacer la compra'},
       {nombre: 'Grabar episodio'},
@@ -15,8 +16,24 @@ var app = new Vue({
 
   methods: {
     guardarTarea: function (tarea) {
-      this.tareas.push({nombre: tarea});
-      this.nombre = "";
+      if (tarea.trim())
+      {
+        this.tareas.push({nombre: tarea});
+        this.nuevaTarea = "";
+      }
+      else
+      {
+        alert("Introduce una tarea");
+      }
+
+
+    },
+
+    eliminarTarea: function(item) {
+      var index = this.tareas.indexOf(item);
+      if(confirm('Eliminar ' + item.nombre + '?') )
+        this.tareas.splice(index, 1);
+
 
     }
   }
