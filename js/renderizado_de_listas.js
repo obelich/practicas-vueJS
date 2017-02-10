94ff4a61809be100710a7b7a0809d347a286f3ca
Obelich <obelich@gmail.com>
2017-02-09 21:03:23 -0800

    Estudiando la renderizacion de listas

---
+++ js/renderizado_de_listas.js
@@ -0,0 +1,15 @@
+/**
+ * Created by obelich on 7/02/17.
+ */
+vm = new Vue({
+  el: 'main',
+
+  data: {
+    laborales: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'],
+    tareas: [
+      {nombre: 'Hacer la compra', prioridad: 'Baja'},
+      {nombre: 'Aprender VUE y Firebase', prioridad: 'Alta'},
+      {nombre: 'Ir al gimnasio', prioridad: 'Alta'}
+    ]
+  }
+});
