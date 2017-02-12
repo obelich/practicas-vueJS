e47cbf2f506100507b2421eca5681850128894ef
Obelich <obelich@gmail.com>
2017-02-12 11:48:55 -0800

    se inicializo la parte de los filtros

---
+++ js/filtros.js
@@ -0,0 +1,40 @@
+/**
+ * Created by obelich on 12/02/17.
+ */
+
+var app = new Vue({
+  el: 'main',
+  data: {
+    message: 'Hello Vue!',
+    minimo: 8,
+    juegos: [
+      {
+        titulo: 'Counter Strike GO',
+        genero: 'fps',
+        puntuacion: 9,
+      },
+      {
+        titulo: 'Minecraft PE',
+        genero: 'sansbox',
+        puntuacion: 9,
+      },
+      {
+        titulo: 'Pokemon',
+        genero: 'rpg',
+        puntuacion: 9,
+      },
+      {
+        titulo: 'Capitan comando',
+        genero: 'plataformas',
+        puntuacion: 7,
+      },
+    ]
+  },
+
+  computed: {
+    mejoresJuegos() {
+      return this.juegos.filter((juego) => juego.puntuacion >= this.minimo);
+    }
+  }
+
+});
